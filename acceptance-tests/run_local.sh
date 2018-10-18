9dcc154b5747ce41058c22b6159b87bd513d730d
smahavithana <smahavithana@gmail.com>
2018-10-18 12:24:47 +0100

    Updated acceptance tests

--- acceptance-tests/run_local.sh
+++ acceptance-tests/run_local.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gradle acceptanceTests -Dheadless=false -DbaseUrl=https://qa.dft-bluebadge.org.uk -Dcucumber.options="--tags @SubmitApplicationAFCSRoute"
+gradle acceptanceTests -Dheadless=false -DbaseUrl=https://qa.dft-bluebadge.org.uk
