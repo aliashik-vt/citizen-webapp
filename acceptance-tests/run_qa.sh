6d9152dbcbbb74b4d683d43a630fa327d02c4792
smahavithana <smahavithana@gmail.com>
2018-11-09 14:09:52 +0000

    Updated test cases to use new radio button control

--- acceptance-tests/run_qa.sh
+++ acceptance-tests/run_qa.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gradle acceptanceTests -Dheadless=false -DbaseUrl=https://qa.dft-bluebadge.org.uk -Dcucumber.options="--tags @PageValidations"
+gradle acceptanceTests -Dheadless=false -DbaseUrl=https://qa.dft-bluebadge.org.uk
+#-DbStackMode=false -DbStackBrowserName="ie" -DbStackBrowserVersion="11.0"
+#-Dcucumber.options="--tags @SubmitApplicationWPMSRoute"
